ab4b9891374d8047be2c66bcdfc959162feb6bcf
A. Marcia Barbosa <ana.marcia.barbosa@gmail.com>
2024-01-19 17:30:02 +0000

    version 3.13.3

--- R/confusionLabel.R
+++ R/confusionLabel.R
@@ -1,5 +1,5 @@
-confusionLabel <- function(model = NULL, obs = NULL, pred = NULL, thresh, interval = 0.01, quant = 0, verbosity = 2, na.rm = FALSE, rm.dup = FALSE) {
-  # version 1.7 (14 Jun 2022)
+confusionLabel <- function(model = NULL, obs = NULL, pred = NULL, thresh, interval = 0.01, quant = 0, verbosity = 2, na.rm = FALSE, rm.dup = FALSE, plot = TRUE, ...) {
+  # version 1.9 (17 Jan 2024)
 
   pred_in <- pred  # in case input is raster, so final reclass is also raster
 
@@ -7,21 +7,35 @@ confusionLabel <- function(model = NULL, obs = NULL, pred = NULL, thresh, interv
   obs <- obspred[ , "obs"]
   pred <- obspred[ , "pred"]
 
-  if (!(is.numeric(thresh) || thresh %in% modEvAmethods("getThreshold")))
-    stop("'thresh' must be either a numeric value between 0 and 1, or one of the options obtained with modEvAmethods('getThreshold')")
+  if (!(is.numeric(thresh) || thresh %in% modEvAmethods("getThreshold")))  stop("'thresh' must be either a numeric value between 0 and 1, or one of the options obtained with modEvAmethods('getThreshold')")
   if (thresh %in% modEvAmethods("getThreshold"))  thresh <- getThreshold(obs = obs, pred = pred, threshMethod = thresh, interval = interval, quant = quant, na.rm = na.rm)
 
-  res <- rep("", length(obs))
-  res[pred >= thresh & obs == 1] <- "TruePos"
-  res[pred < thresh & obs == 0] <- "TrueNeg"
-  res[pred >= thresh & obs == 0] <- "FalsePos"
-  res[pred < thresh & obs == 1] <- "FalseNeg"
+  out_chr <- rep("", length(obs))
+  out_chr[pred >= thresh & obs == 1] <- "TruePos"
+  out_chr[pred < thresh & obs == 0] <- "TrueNeg"
+  out_chr[pred >= thresh & obs == 0] <- "FalsePos"
+  out_chr[pred < thresh & obs == 1] <- "FalseNeg"
 
   if (inherits(pred_in, "SpatRaster")) {
-    finite_pixels <- is.finite(terra::values(pred_in))
-    terra::values(pred_in)[finite_pixels] <- as.integer(as.factor(res))
-    res <- pred_in
+    finite_pixels <- which(is.finite(terra::values(pred_in)))
+    out_rast <- pred_in
+    levs <- as.factor(out_chr)
+    terra::values(out_rast)[finite_pixels] <- levs  # only 'finite_pixels' because out_chr vector is shorter when input has NAs
+
+    # convert to categorical raster:
+    levels(out_rast) <- data.frame(id = as.integer(unique(levs)), cat = unique(out_chr))
+
+    # set colours for raster categories:
+    colr_table <- data.frame(lev = as.factor(c("TruePos", "FalsePos", "TrueNeg", "FalseNeg")), col = c("royalblue", "lightblue", "red", "orange"))
+    existing_levs <- colr_table$lev %in% levels(out_rast)[[1]]$cat
+    terra::coltab(out_rast) <- data.frame(
+      values = droplevels(colr_table$lev[existing_levs]),
+      cols = colr_table$col[existing_levs])
+
+    if (plot) terra::plot(out_rast, ...)
+
+    return(out_rast)
   }
 
-  res
+  return(out_chr)
 }
